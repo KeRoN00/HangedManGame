1a6e183eff182e0e65bbf85c3fcffeb015d8bfda
KeRoN00 <listwon.dev@gmail.com>
2023-02-21 17:57:08 +0100

    Initial commit

---
+++ HangedManGame/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  //TODO:  Create a basic layout ( word, hangedman image, keyboard, scoreboard, info, HowToPlay)
+
+  return (
+    <div className="App">
+      HangedManGame (in progress...)
+    </div>
+  )
+}
+
+export default App
